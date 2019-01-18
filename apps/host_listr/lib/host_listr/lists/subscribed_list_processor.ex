f5c50ee000258bcbf49d1556e0e20974625b2dd1
David Oliver <david@doliver.co.uk>
2019-01-18 18:51:07 +0100

    Move public function above private functions

--- apps/host_listr/lib/host_listr/lists/subscribed_list_processor.ex
+++ apps/host_listr/lib/host_listr/lists/subscribed_list_processor.ex
@@ -4,17 +4,6 @@ defmodule HostListr.Lists.SubscribedListProcessor do
 
   @localhost_hosts_with_dots ~w(0.0.0.0 localhost.localdomain)s
 
-  defp process_flow(flow) do
-    flow
-    |> Flow.map(&uncomment_line/1)
-    |> Flow.reject(&empty?/1)
-    |> Flow.map(&parse_hosts_format/1)
-    |> Flow.filter(&contains_dot?/1)
-    |> Flow.map(&parse_url/1)
-    |> Flow.reject(&localhost?/1)
-    |> Enum.join("\n")
-  end
-
   # text, potentially multi-line
   def process(hosts_source) when is_binary(hosts_source) do
     hosts_source
@@ -29,6 +18,17 @@ defmodule HostListr.Lists.SubscribedListProcessor do
     hosts_source
     |> Flow.from_stages()
     |> process_flow()
+  end
+
+  defp process_flow(flow) do
+    flow
+    |> Flow.map(&uncomment_line/1)
+    |> Flow.reject(&empty?/1)
+    |> Flow.map(&parse_hosts_format/1)
+    |> Flow.filter(&contains_dot?/1)
+    |> Flow.map(&parse_url/1)
+    |> Flow.reject(&localhost?/1)
+    |> Enum.join("\n")
   end
 
   defp uncomment_line(line) when is_binary(line) do
